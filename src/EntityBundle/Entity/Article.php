47e54a9c78d0d67df3e46f1da0a0c98b836f812c
babachir <user.name>
2015-12-21 02:55:34 +0100

    get Json of article collection , google map view

--- src/EntityBundle/Entity/Article.php
+++ src/EntityBundle/Entity/Article.php
@@ -259,5 +259,7 @@ class Article
     }
 
 
+
+
 }
 
--- src/PublicBundle/Controller/ArticleController.php
+++ src/PublicBundle/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace PublicBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \DateTime;
 
 class ArticleController extends Controller
 {
@@ -13,7 +14,28 @@ class ArticleController extends Controller
 
     public function readAction($id)
     {
+
         return $this->render('PublicBundle:Article:read.html.twig', array('id'  => $id));
+    }
+    public function getjsonarticlesAction($year)
+    {
+
+        $articleRepository = $this->getDoctrine()->getManager()->getRepository('EntityBundle:Article');
+        $date = new DateTime($year.'-01-01');
+
+        $datas=$articleRepository->findAll();
+        $arrayArticle = array();
+
+        foreach($datas as $data )
+        {
+                $arrayArticle[]= array('titre' => $data->getTitre(),'DateDebut' => $data->getDateDebut()->format("Y"),
+                    'DateFin' => $data->getDateFin()->format("Y"),'place' => $data->getLocalisation()->getNom(),
+                    'longitude' => $data->getLocalisation()->getLongitude(),'latitude' => $data->getLocalisation()->getLatitude());
+
+        }
+
+        $response = new \Symfony\Component\HttpFoundation\Response(json_encode($arrayArticle));
+        return $response;
     }
 
 
