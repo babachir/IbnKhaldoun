0c0f62833e8377df64639f1c87a698cb3436d17c
babachir <user.name>
2015-12-24 01:42:38 +0100

    commentaire

--- src/PublicBundle/Controller/ArticleController.php
+++ src/PublicBundle/Controller/ArticleController.php
@@ -4,6 +4,10 @@ namespace PublicBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use \DateTime;
+use EntityBundle\Entity\Commentaire;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type;
 
 class ArticleController extends Controller
 {
@@ -12,10 +16,39 @@ class ArticleController extends Controller
         return $this->render('PublicBundle:Article:index.html.twig');
     }
 
-    public function readAction($id)
+    public function readAction($id  ,Request $request)
     {
+        $session = $request->getSession();
+        $ArticleRepository = $this->getDoctrine()->getManager()->getRepository('EntityBundle:Article');
 
-        return $this->render('PublicBundle:Article:read.html.twig', array('id'  => $id));
+        $article = $ArticleRepository->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $commentaire = new Commentaire();
+
+        $form = $this->createFormBuilder($commentaire)
+                ->add('pseudo',TextType::class)
+                ->add('contenu',TextType::class)
+                ->add('commenter', Type\SubmitType::class)
+                ->getForm();
+        $form->handleRequest($request);
+        if($form->isValid())
+        {
+            /**/
+            $commentaire->setIsvalide('0');
+            $commentaire->setArticle($article);
+            $em->persist($commentaire);
+            $em->flush();
+            $this->addFlash(
+                'notice',
+                'Commentaire en attente de validation par l\'Administratue!'
+            );
+            return $this->redirect($this->generateUrl("public_readarticle",array('id' => $article->getId())));
+            //return $this->render('PublicBundle:Article:read.html.twig', array('article'  => $article ,'form'=> $form->createView()));
+        }
+
+
+
+        return $this->render('PublicBundle:Article:read.html.twig', array('article'  => $article ,'form'=> $form->createView()));
     }
     public function getjsonarticlesAction($year)
     {
